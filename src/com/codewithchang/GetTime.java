7b16e9e372a3cc9e3baf0ce36cf71824f9dc36e1
Chang <cnxiong03@gmail.com>
2021-03-09 23:25:15 -0500

    if time is less than 10 add 0 to our time

--- src/com/codewithchang/GetTime.java
+++ src/com/codewithchang/GetTime.java
@@ -8,7 +8,24 @@ public class GetTime {
         }
         long hours = minutes / 60;
         long remainingMinutes = minutes % 60;
-        return hours + "h " + remainingMinutes + "m " + seconds + "s";
+
+        String hoursString = hours + "h";
+        if(hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if(remainingMinutes < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+        if(seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+
+        return hoursString + " " + minutesString + " " + secondsString;
     }
 
     public static String getDurationString(long seconds) {
